0e8b7083580a78916392b9006582cd6b1166c12b
shafik720 <shafikrasel5@gmail.com>
2022-10-17 17:07:39 +0600

    sdfsf

---
+++ app2.js
@@ -0,0 +1,50 @@
+
+const tetrisParent = document.querySelector('.tetris-parent'),
+grid = tetrisParent.querySelectorAll('div'),
+width = 10,
+div = Array.from(grid);
+// console.log(div);
+
+
+const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2]
+  ]
+
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1]
+  ]
+
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1]
+  ]
+
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1]
+  ]
+
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+  ]
+
+
+let tetrisContainer = [lTetromino,zTetromino,tTetromino,oTetromino, iTetromino];
+let random = Math.floor(Math.random() * tetrisContainer.length);
+
+let container = tetrisContainer[random][0];
+console.log(container);
+container.forEach(index=>div[index+4].classList.add('tetromino'))
