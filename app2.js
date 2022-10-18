52ab4fab015f00f42df23117ba1b37cfb92d5e16
shafik720 <shafikrasel5@gmail.com>
2022-10-18 20:08:05 +0600

    sdfssfv

--- app2.js
+++ app2.js
@@ -1,12 +1,4 @@
 
-let parentDiv = document.querySelector('.tetris-parent');
-let div = parentDiv.querySelectorAll('div');
-let currentPosition = 4 ;
-let width = 10;
-let currentRotation = 0;
-
-let grid = Array.from(div);
-
 // lTetromino 
 const lTetromino = [
   [1, width + 1, width * 2 + 1, 2],
@@ -43,21 +35,6 @@ const iTetromino = [
   [width, width + 1, width + 2, width + 3]
 ]
 
-let containers = [lTetromino,zTetromino,tTetromino,oTetromino,iTetromino];
-let random = Math.floor(Math.random() * containers.length)
-let containersTwo = containers[random][currentRotation];
+let parentDiv = document.querySelector('.tetris-parent'),
+grid = parentDiv.querySelectorAll('div');
 
-function draw(){
-  containersTwo.some(index=>grid[index + currentPosition].classList.add('blue'));
-}
-draw();
-function unDraw(){
-  containersTwo.some(index=>grid[index + currentPosition].classList.remove('blue'));
-}
-
-function run(){
-  unDraw();
-  currentPosition += width;
-  draw();
-}
-setInterval(run,1000)
