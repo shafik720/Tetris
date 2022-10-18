55c7bf18efd2fb08f0db44be9be9a12253003fc2
shafik720 <shafikrasel5@gmail.com>
2022-10-18 22:09:27 +0600

    sdfgfdsg

--- app2.js
+++ app2.js
@@ -1,4 +1,4 @@
-
+let width = 10;
 // lTetromino 
 const lTetromino = [
   [1, width + 1, width * 2 + 1, 2],
@@ -36,5 +36,44 @@ const iTetromino = [
 ]
 
 let parentDiv = document.querySelector('.tetris-parent'),
-grid = parentDiv.querySelectorAll('div');
+currentPosition = 4,
+currentRotation = 0,
+grid = parentDiv.querySelectorAll('div'),
+squares = Array.from(document.querySelectorAll('.tetris-parent div'))
+;
 
+let theTetrominoes = [lTetromino,zTetromino,tTetromino,oTetromino,iTetromino];
+let random = Math.floor(Math.random() * theTetrominoes.length);
+let current = theTetrominoes[random][currentRotation];
+
+function draw (){
+  current.forEach(index=>{
+      squares[currentPosition+index].classList.add('tetromino');
+  })
+}
+function unDraw (){
+  current.forEach(index=>{
+      squares[currentPosition+index].classList.remove('tetromino');
+  })
+}
+
+let timedraw = setInterval(moveDown,500)
+
+draw();
+
+function moveDown(){
+  unDraw();
+  currentPosition += width;
+  draw();
+  freeze();
+}
+
+function freeze(){
+  if(current.some(index=>squares[index + currentPosition + width].classList.contains('taken'))){
+    current.forEach(index=>squares[currentPosition+index].classList.add('taken'));
+    random = Math.floor(Math.random() * theTetrominoes.length);
+    current = theTetrominoes[random][currentRotation];
+    currentPosition = 4 ;
+    draw();
+  }
+}
