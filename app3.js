114d008472dd2e2c9a2416cf0ffdf04e647436bf
shafik720 <shafikrasel5@gmail.com>
2022-10-22 20:41:53 +0600

    sfgfdg

--- app3.js
+++ app3.js
@@ -117,12 +117,10 @@ function rotate(){
 
     current = tetrominos[random][currentRotation];
     if(current.some(index=>squares[currentPosition + index].classList.contains('taken'))){
-        console.log(random);
         if(random==3){
             console.log('ok first');
             if(current.some(index=>squares[index + currentPosition +1].classList.contains('taken'))){
                 currentPosition -= 1;  
-                console.log('ok final');
             }
         }
         --currentRotation;
