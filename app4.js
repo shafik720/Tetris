0a1bf4d59604b5e1019391ce9c20d871e28906fc
shafik720 <shafikrasel5@gmail.com>
2022-10-24 17:47:31 +0600

    dfgfdg

--- app4.js
+++ app4.js
@@ -5,3 +5,47 @@ width = 10,
 currentRotation = 0,
 currentPosition = 4 
 ;
+
+// lTetromino 
+const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2]
+  ]
+
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1]
+  ]
+
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1]
+  ]
+
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1]
+  ]
+
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+  ]
+
+let allTetromino = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+let random = Math.floor(Math.random() * allTetromino.length);
+let present = allTetromino[random][currentRotation];
+
+const draw = () =>{
+    
+}
