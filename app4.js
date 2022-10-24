2f256058d95d0352b8d8d4381fb0defe657951c5
shafik720 <shafikrasel5@gmail.com>
2022-10-24 17:44:32 +0600

    dfgfdg

---
+++ app4.js
@@ -0,0 +1,7 @@
+
+let grid = document.querySelector('.tetris-parent'),
+containers = Array.from(document.querySelectorAll('.tetris-parent div')),
+width = 10,
+currentRotation = 0,
+currentPosition = 4 
+;
